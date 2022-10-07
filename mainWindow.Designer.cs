e406c1fcd13ffe1828960f9aab1b0091b809ba69
bjadczak <73365815+bjadczak@users.noreply.github.com>
2022-10-07 15:10:20 +0200

    Reorganise layout of the window

--- mainWindow.Designer.cs
+++ mainWindow.Designer.cs
@@ -28,59 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.mainPictureBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.mainPictureBox);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 614;
-            this.splitContainer1.TabIndex = 0;
             // 
             // mainPictureBox
             // 
             this.mainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 24);
             this.mainPictureBox.Name = "mainPictureBox";
-            this.mainPictureBox.Size = new System.Drawing.Size(614, 450);
-            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.Size = new System.Drawing.Size(800, 426);
+            this.mainPictureBox.TabIndex = 1;
             this.mainPictureBox.TabStop = false;
-            this.mainPictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.clickOnPictureBox);
+            this.mainPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.clickOnPictureBox);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // mainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.mainPictureBox);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "mainWindow";
             this.Text = "poligonEditor";
             this.Resize += new System.EventHandler(this.mainWindow_Resize);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
